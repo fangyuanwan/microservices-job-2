1c3b35f8938987ef2bf92b01b44709def55f5376
fangyuanwan <fangyuanwan1989@gmail.com>
2021-11-18 14:35:23 +0800

    Add nu packages,models,context and booking record class attributes

---
+++ BookingRecordApi/Models/BookingRecordContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace BookingRecordApi.Models
+{
+    public class BookingRecordContext:DbContext
+
+    {
+        public BookingRecordContext(DbContextOptions<BookingRecordContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BookingRecordItem> BookingRecordItems { get; set; }
+    }
+}
---
+++ BookingRecordApi/Models/BookingRecordItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BookingRecordApi.Models
+{
+    public class BookingRecordItem
+    {
+       public DateTime datetime { get; set; }
+       public string Date { get; set; }
+       public string Time { get; set; }
+       public string PickupPoint { get; set; }
+       public string Destination { get; set; }
+       public float CurrentLocationLat { get; set; }
+       public float CurrentLocationLon { get; set; }
+
+    }
+}
